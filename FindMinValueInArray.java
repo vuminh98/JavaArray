fbcaf0b89106cd8280ebcbfc96292a2225fdb208
vuminh98 <vuminhtran27@gmail.com>
2022-10-09 13:11:34 +0700

    a

--- FindMinValueInArray.java
+++ FindMinValueInArray.java
@@ -1,5 +1,19 @@
 public class FindMinValueInArray {
     public static void main(String[] args) {
-
+        System.out.println("Please input numbers:");
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+        System.out.println("Value Min is : ");
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
     }
 }
